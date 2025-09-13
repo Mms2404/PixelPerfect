c26490ec430dd7db77d964b3bd3f200f3e4daa8b
Madhumita <madhumitasri24@gmail.com>
2025-09-13 11:15:12 +0530

    'Initiated'

---
+++ pixelperfect/lib/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryPurple = Color(0xFF5E1053);
+  static const Color brown = Color(0xFF5B3700);
+  static const Color orange = Color(0xFFA16000);
+  static const Color grey = Color(0xFF6E6F73);
+  static const Color darkGrey = Color(0xFF22242B);
+}
---
+++ pixelperfect/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(393, 853), // figma screen size
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Internship Task',
+          home: child,
+        );
+      },
+      child: const Placeholder(),
+    );
+  }
+}
